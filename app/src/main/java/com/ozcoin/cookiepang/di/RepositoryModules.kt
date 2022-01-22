79ca37e7f16678253988d4376c3c284a57163b7c
DwEnn <prgohn@gmail.com>
2022-01-22 19:22:24 +0900

    feat 화면 추가

--- app/src/main/java/com/ozcoin/cookiepang/di/RepositoryModules.kt
+++ app/src/main/java/com/ozcoin/cookiepang/di/RepositoryModules.kt
@@ -12,6 +12,7 @@ val repositoryModule = module {
             themeModeLocalDataSource = get()
         )
     }
+
     factory<UserRegRepository> {
         UserRegRepositoryImpl()
     }
---
+++ app/src/main/java/com/ozcoin/cookiepang/ui/selectcategory/SelectCategoryFragment.kt
@@ -0,0 +1,13 @@
+package com.ozcoin.cookiepang.ui.selectcategory
+
+import com.ozcoin.cookiepang.R
+import com.ozcoin.cookiepang.base.BaseFragment
+import com.ozcoin.cookiepang.databinding.FragmentSelectCategoryBinding
+
+class SelectCategoryFragment: BaseFragment<FragmentSelectCategoryBinding>() {
+    override fun getLayoutRes(): Int {
+        return R.layout.fragment_select_category
+    }
+
+
+}
