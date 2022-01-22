2e13c13b56ab1f1a86c8f6f5bb725d7a5ca7b944
DwEnn <prgohn@gmail.com>
2022-01-22 19:21:51 +0900

    refactor main 화면 구조 변경

--- app/src/main/java/com/ozcoin/cookiepang/ui/MainActivityViewModel.kt
+++ app/src/main/java/com/ozcoin/cookiepang/ui/MainActivityViewModel.kt
@@ -1,25 +1,9 @@
 package com.ozcoin.cookiepang.ui
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import com.ozcoin.cookiepang.base.BaseViewModel
 
 class MainActivityViewModel : BaseViewModel() {
 
-    private val _useTitleBar = MutableLiveData<Boolean>().also { it.value = false }
-    val useTitleBar : LiveData<Boolean>
-        get() = _useTitleBar
 
-    private val _useBottomNav = MutableLiveData<Boolean>().also { it.value = false }
-    val useBottomNav : LiveData<Boolean>
-        get() = _useBottomNav
-
-    fun setUseTitleBar(useTitleBar: Boolean) {
-        _useTitleBar.postValue(useTitleBar)
-    }
-
-    fun setUseBottomNav(useBottomNav: Boolean) {
-        _useBottomNav.postValue(useBottomNav)
-    }
 
 }
