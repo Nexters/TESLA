3ee6de83aef92ef0dd21b19f2de1f3e1cd701295
DwEnn <prgohn@gmail.com>
2022-02-16 11:51:50 +0900

    feat: edit cookie fragment, viewmodel 추가

--- app/src/main/java/com/ozcoin/cookiepang/ui/MainActivity.kt
+++ app/src/main/java/com/ozcoin/cookiepang/ui/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.ozcoin.cookiepang.ui
 
+import android.view.animation.AnimationUtils
 import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.navOptions
 import androidx.navigation.ui.setupWithNavController
 import com.ozcoin.cookiepang.R
 import com.ozcoin.cookiepang.base.BaseActivity
 import com.ozcoin.cookiepang.databinding.ActivityMainBinding
+import com.ozcoin.cookiepang.utils.Event
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
@@ -43,12 +46,8 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     }
 
     private fun setNavDestinationListener() {
-        navController.addOnDestinationChangedListener { controller, destination, arguments ->
-            Timber.d("destination : ${destination.navigatorName}")
-
-            supportFragmentManager.fragments.forEachIndexed { index, fragment ->
-                Timber.d("$index : ${fragment.javaClass.simpleName}")
-            }
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            mainActivityViewModel.setCurrentFragmentName(destinationId = destination.id)
         }
     }
 
@@ -64,5 +63,34 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     }
 
     override fun init() {
+    }
+
+    override fun handleFabAnim(event: Event.FabAnim) {
+        Timber.d("handleFabAnim(${event.javaClass.name})")
+        val animRes = if (event is Event.FabAnim.Left) {
+            binding.ivFabBtn.rotation = 405f
+            R.anim.rotate_0_to_405
+        } else {
+            binding.ivFabBtn.rotation = 0f
+            R.anim.rotate_405_to_0
+        }
+
+        binding.ivFabBtn.startAnimation(
+            AnimationUtils.loadAnimation(
+                this, animRes
+            )
+        )
+    }
+
+    override fun handleNavToEditCookie() {
+        val options = navOptions {
+            anim {
+                enter = R.anim.slide_in_up
+                exit = R.anim.slide_out_down
+                popEnter = R.anim.slide_in_up
+                popExit = R.anim.slide_out_down
+            }
+        }
+        navController.navigate(R.id.editCookie_dest, null, options)
     }
 }
--- app/src/main/java/com/ozcoin/cookiepang/ui/MainActivityViewModel.kt
+++ app/src/main/java/com/ozcoin/cookiepang/ui/MainActivityViewModel.kt
@@ -2,12 +2,15 @@ package com.ozcoin.cookiepang.ui
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
+import com.ozcoin.cookiepang.R
 import com.ozcoin.cookiepang.base.BaseViewModel
+import com.ozcoin.cookiepang.utils.Event
 import com.ozcoin.cookiepang.utils.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
@@ -18,6 +21,38 @@ class MainActivityViewModel @Inject constructor(
     private val _isBtmNavViewVisible = MutableStateFlow(true)
     val isBtmNavViewVisible: StateFlow<Boolean>
         get() = _isBtmNavViewVisible
+
+    private val _isEditingCookie = MutableStateFlow(false)
+    val isEditingCookie: StateFlow<Boolean>
+        get() = _isEditingCookie
+
+    private var currentFragmentId = -1
+        set(value) {
+            if (field != R.id.editCookie_dest && value == R.id.editCookie_dest) {
+                isEditingCookie(true)
+                animFab(Event.FabAnim.Left)
+            } else if (field == R.id.editCookie_dest && value != R.id.editCookie_dest) {
+                isEditingCookie(false)
+                animFab(Event.FabAnim.Right)
+            }
+            field = value
+        }
+
+    private fun animFab(event: Event.FabAnim) {
+        viewModelScope.launch {
+            _eventFlow.emit(event)
+        }
+    }
+
+    private fun isEditingCookie(isEditingCookie: Boolean) {
+        viewModelScope.launch {
+            _isEditingCookie.emit(isEditingCookie)
+        }
+    }
+
+    fun setCurrentFragmentName(destinationId: Int) {
+        currentFragmentId = destinationId
+    }
 
     fun showBtmNavView() {
         viewModelScope.launch { _isBtmNavViewVisible.emit(true) }
@@ -31,5 +66,28 @@ class MainActivityViewModel @Inject constructor(
         viewModelScope.launch {
             _uiStateFlow.emit(uiState)
         }
+    }
+
+    private fun navigateToEditCookie() {
+        viewModelScope.launch {
+            _eventFlow.emit(Event.Nav.ToEditCookie)
+        }
+    }
+
+    private fun showCancelToEditingCookieDialog() {
+        viewModelScope.launch {
+        }
+    }
+
+    fun clickFabBtn() {
+        if (!isEditingCookie.value)
+            navigateToEditCookie()
+        else
+            showCancelToEditingCookieDialog()
+    }
+
+    fun clickInterceptBtnMenu() {
+        Timber.d("Btm menu is intercepted haha")
+        showCancelToEditingCookieDialog()
     }
 }
