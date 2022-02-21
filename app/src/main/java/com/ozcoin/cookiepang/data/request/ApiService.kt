438013295e7c2b89b19d1d70d8cd3aa9921e79c4
DwEnn <prgohn@gmail.com>
2022-02-22 03:04:12 +0900

    api 스펙 적용

--- app/src/main/java/com/ozcoin/cookiepang/data/request/ApiService.kt
+++ app/src/main/java/com/ozcoin/cookiepang/data/request/ApiService.kt
@@ -1,10 +1,19 @@
 package com.ozcoin.cookiepang.data.request
 
 import com.ozcoin.cookiepang.data.ask.AskEntity
+import com.ozcoin.cookiepang.data.ask.AskUpdateRequestParam
 import com.ozcoin.cookiepang.data.category.CategoryEntity
 import com.ozcoin.cookiepang.data.cookie.CookieEntity
+import com.ozcoin.cookiepang.data.cookie.CookieListResponse
 import com.ozcoin.cookiepang.data.cookie.MakeACookieRequestParam
+import com.ozcoin.cookiepang.data.cookie.UpdateCookieInfoRequestParam
 import com.ozcoin.cookiepang.data.cookiedetail.CookieDetailEntity
+import com.ozcoin.cookiepang.data.notification.NotificationEntity
+import com.ozcoin.cookiepang.data.response.AddressResponse
+import com.ozcoin.cookiepang.data.response.AmountResponse
+import com.ozcoin.cookiepang.data.response.AnswerResponse
+import com.ozcoin.cookiepang.data.response.PriceResponse
+import com.ozcoin.cookiepang.data.response.TokenAddressResponse
 import com.ozcoin.cookiepang.data.timeline.TimeLineEntity
 import com.ozcoin.cookiepang.data.user.UserEntity
 import retrofit2.Response
@@ -21,6 +30,10 @@ interface ApiService {
     @GET("/auth")
     suspend fun auth(): Response<Unit>
 
+    /*
+        user-controller
+     */
+
     @POST("/users")
     suspend fun registrationUser(@Body userEntity: UserEntity): Response<UserEntity>
 
@@ -33,17 +46,9 @@ interface ApiService {
         @Body introduction: String
     ): Response<UserEntity>
 
-    @POST("/users/{userId}/categories")
-    suspend fun setInterestInCategoryList(
-        @Path("userId") userId: Int,
-        @Body categoryIdList: List<Int>
-    ): Response<Unit>
-
-    @GET("/categories")
-    suspend fun getAllCategoryList(): Response<List<CategoryEntity>>
-
-    @POST("/asks")
-    suspend fun sendAsk(@Body askEntity: AskEntity): Response<AskEntity>
+    /*
+        cookie-controller
+     */
 
     @POST("/cookies")
     suspend fun makeACookie(
@@ -53,43 +58,130 @@ interface ApiService {
     @PUT("/cookies/{cookieId}")
     suspend fun updateCookieInfo(
         @Path("cookieId") cookieId: String,
-        @Body price: Int,
-        @Body status: String,
-        @Body purchaserUserId: Int
+        @Body updateCookieInfoRequestParam: UpdateCookieInfoRequestParam
     ): Response<CookieEntity>
 
     @DELETE("/cookies/{cookieId}")
     suspend fun deleteCookie(@Path("cookieId") cookieId: String): Response<Unit>
 
-    @POST("/cookies")
-    suspend fun editCookie(
-        @Body question: String,
-        @Body answer: String,
-        @Body price: Int,
-        @Body authorUserId: Int,
-        @Body ownedUserId: Int,
-        @Body txHash: String,
-        @Body categoryId: Int
-    ): Response<CookieEntity>
-
-    @GET("/users/{userId}/categories/all/cookies")
-    suspend fun getAllCookieList(
-        @Path("userId") userId: String,
-        @Query("page") page: Int = 0,
-        @Query("size") size: Int = 50
-    ): Response<List<TimeLineEntity>>
-
-    @GET("/users/{userId}/categories/{categoryId}/cookies")
+    @GET("/users/{userId}/cookies")
     suspend fun getCookieList(
-        @Path("userId") userId: String,
-        @Path("categoryId") categoryId: String,
+        @Path("userId") userId: Int,
+        @Query("target") target: String,
         @Query("page") page: Int = 0,
-        @Query("size") size: Int = 50
-    ): Response<List<TimeLineEntity>>
+        @Query("size") size: Int = 3
+    ): Response<CookieListResponse>
+
+    /*
+        ask-controller
+     */
+
+    @POST("/asks")
+    suspend fun sendAsk(@Body askEntity: AskEntity): Response<AskEntity>
+
+    @GET("/users/{userId}/asks")
+    suspend fun getAskList(
+        @Path("userId") userId: Int,
+        @Query("target") target: String
+    ): Response<List<AskEntity>>
+
+    @PUT("/asks/{askId}")
+    suspend fun updateAsk(
+        @Path("askId") askId: String,
+        @Query("updateAsk") updateRequestParam: AskUpdateRequestParam
+    ): Response<AskEntity>
+
+    /*
+        category-controller
+     */
+
+    @POST("/users/{userId}/categories")
+    suspend fun setInterestInCategoryList(
+        @Path("userId") userId: Int,
+        @Body categoryIdList: List<Int>
+    ): Response<Unit>
+
+    @GET("/categories")
+    suspend fun getAllCategoryList(): Response<List<CategoryEntity>>
+
+    /*
+        notification-controller
+     */
+
+    @GET("/users/{userId}/notifications")
+    suspend fun getAlarmList(
+        @Path("userId") userId: Int,
+        @Query("page") page: Int = 0,
+        @Query("size") size: Int = 3
+    ): Response<List<NotificationEntity>>
+
+    /*
+        view-controller
+     */
 
     @GET("/users/{userId}/cookies/{cookieId}/detail")
     suspend fun getCookieDetail(
         @Path("userId") userId: String,
         @Path("cookieId") cookieId: String
     ): Response<CookieDetailEntity>
+
+    @GET("/users/{userId}/categories/{categoryId}/cookies")
+    suspend fun getCookieList(
+        @Path("userId") userId: String,
+        @Path("categoryId") categoryId: String,
+        @Query("page") page: Int = 0,
+        @Query("size") size: Int = 3
+    ): Response<List<TimeLineEntity>>
+
+    @GET("/users/{userId}/categories/all/cookies")
+    suspend fun getAllCookieList(
+        @Path("userId") userId: String,
+        @Query("page") page: Int = 0,
+        @Query("size") size: Int = 3
+    ): Response<List<TimeLineEntity>>
+
+    /*
+        contract-hammer-controller
+     */
+
+    @GET("/contract/hammers/users/{userId}/count")
+    suspend fun getNumOfHammer(
+        @Path("userId") userId: Int
+    ): Response<AmountResponse>
+
+    @GET("/contract/hammers/users/{userId}/approve")
+    suspend fun getIsWalletApproved(
+        @Path("userId") userId: Int
+    ): Response<AnswerResponse>
+
+    @GET("/contract/hammers/address")
+    suspend fun getHammerContractAddress(): Response<AddressResponse>
+
+    /*
+        contract-cookie-controller
+     */
+
+    @GET("/contract/cookies/{id}/sale")
+    suspend fun isOnSaleCookie(
+        @Path("id") id: Int
+    ): Response<AnswerResponse>
+
+    @GET("/contract/cookies/{id}/price")
+    suspend fun getCookiePrice(
+        @Query("nftTokenId") nftTokenId: String
+    ): Response<PriceResponse>
+
+    @GET("/contract/cookies/{id}/hide")
+    suspend fun isCookieHidden(
+        @Query("id") id: String
+    ): Response<AnswerResponse>
+
+    @GET("/contract/cookies/users/{userId}/nftTokenId")
+    suspend fun getTokenAddress(
+        @Path("userId") userId: String,
+        @Query("index") index: String
+    ): Response<TokenAddressResponse>
+
+    @GET("/contract/cookies/address")
+    suspend fun getCookieContractAddress(): Response<AddressResponse>
 }
