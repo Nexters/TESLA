8832a7dcc663e433989a0bb47010c0a63d99edbe
DwEnn <prgohn@gmail.com>
2022-02-24 04:22:03 +0900

    refactor: cookie imageUrl 변경

--- app/src/main/java/com/ozcoin/cookiepang/domain/feed/CookieCardStyle.kt
+++ app/src/main/java/com/ozcoin/cookiepang/domain/feed/CookieCardStyle.kt
@@ -1,8 +1,8 @@
 package com.ozcoin.cookiepang.domain.feed
 
 enum class CookieCardStyle(val imageUrl: String) {
-    PINK("https://cdn.cookiepang.site/metadata/cookie_01.json"),
-    BLUE("https://cdn.cookiepang.site/metadata/cookie_02.json"),
-    YELLOW("https://cdn.cookiepang.site/metadata/cookie_03.json"),
-    PURPLE("https://cdn.cookiepang.site/metadata/cookie_04.json")
+    PINK("https://cdn.cookiepang.site/metadata/cookie_meta01.json"),
+    BLUE("https://cdn.cookiepang.site/metadata/cookie_meta02.json"),
+    YELLOW("https://cdn.cookiepang.site/metadata/cookie_meta03.json"),
+    PURPLE("https://cdn.cookiepang.site/metadata/cookie_meta04.json")
 }
