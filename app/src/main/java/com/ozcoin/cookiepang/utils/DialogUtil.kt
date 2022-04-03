57053ca75ecaf18c5e0959bb05646fbdea1386db
DwEnn <prgohn@gmail.com>
2022-04-03 18:16:08 +0900

    문구 변경

--- app/src/main/java/com/ozcoin/cookiepang/utils/DialogUtil.kt
+++ app/src/main/java/com/ozcoin/cookiepang/utils/DialogUtil.kt
@@ -72,9 +72,9 @@ object DialogUtil {
 
     fun getMakeCookieFailContents(): DialogContents {
         return DialogContents(
-            title = "쿠키가 굽기가 실패했습니다.",
-            contents = "쿠키 굽기를 알 수 없는 이유로 실패했습니다. \n" +
-                "다시 시도하시겠어요?",
+            title = "쿠키가 굽기를 실패했습니다.",
+            contents = "쿠키 굽기를 실패했어요.\n" +
+                "다시 시도하시겠어요.",
             confirm = "다시 시도하기",
             cancel = "취소하기"
         )
